181addfe493c85512f59e6d726ac83154edc7c6b
jianna-braza <jbraza@uw.edu>
2024-08-21 20:20:14 -0700

    adding aquanauts project information

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -38,7 +38,7 @@ function IndexHeader() {
         <div className="content-center">
           <Container>
             <div className="title-brand">
-              <h1 className="presentation-title">Personal Website</h1>
+              <h1 className="presentation-title">Project Portfolio</h1>
             </div>
             <h2 className="presentation-subtitle text-center">
               Aspiring Front-End Developer
--- src/views/index-sections/AquapodSection.js
+++ src/views/index-sections/AquapodSection.js
@@ -8,6 +8,8 @@ import {
   NavLink,
   Nav,
   Container,
+  Row,
+  Col
 } from "reactstrap";
 
 
@@ -18,17 +20,64 @@ function AquapodSection() {
       <Navbar className="bg-primary" expand="lg">
         <Container>
           <NavbarBrand href="#pablo" onClick={(e) => e.preventDefault()}>
-            Current Projects
+            The Aquapod: Water Resource Locator
           </NavbarBrand>
+          <button
+            aria-controls="navbarNav"
+            aria-expanded={false}
+            aria-label="Toggle navigation"
+            className="navbar-toggler navbar-toggler-right burger-menu"
+            data-target="#navbar-primary"
+            data-toggle="collapse"
+            id="navbar-primary"
+            type="button"
+          >
+            <span className="navbar-toggler-bar" />
+            <span className="navbar-toggler-bar" />
+            <span className="navbar-toggler-bar" />
+          </button>
+          <UncontrolledCollapse navbar toggler="#navbar-primary">
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <i
+                    aria-hidden={true}
+                    className="nc-icon nc-compass-05"
+                  />
+                  Repository
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </UncontrolledCollapse>
         </Container>
       </Navbar>
-      <div className='section section-buttons' >
+
+      <div className='section' >
         <Container>
-          <div className='title' >
-            <h2>
-              hi
-            </h2>
-          </div>
+          <img src='assets/img/aquanautsWebsite.png' />
+        </Container>
+      </div>
+
+
+
+
+
+      <div className="section">
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto text-center" md="8">
+              <h2 className="title">Completed with examples</h2>
+              <p className="description">
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. More importantly, looking at them will give you a picture of
+                what you can built with this powerful kit.
+              </p>
+            </Col>
+          </Row>
         </Container>
       </div>
 
